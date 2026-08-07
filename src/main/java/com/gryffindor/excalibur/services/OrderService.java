89951c642005537222692fcb6f4e266b348baf0d
Jayesh Chaudhari <86295229+jayeshC01@users.noreply.github.com>
2026-08-07 15:18:21 +0530

    Skipped

--- src/main/java/com/gryffindor/excalibur/services/OrderService.java
+++ src/main/java/com/gryffindor/excalibur/services/OrderService.java
@@ -1,102 +1,102 @@
-package com.gryffindor.excalibur.services;
+// package com.gryffindor.excalibur.services;
 
-import com.gryffindor.excalibur.constants.OrderStatus;
-import com.gryffindor.excalibur.models.OrderRequest;
-import com.gryffindor.excalibur.models.db.Order;
-import com.gryffindor.excalibur.models.db.OrderDetails;
-import com.gryffindor.excalibur.models.db.Product;
-import com.gryffindor.excalibur.repository.OrderRepository;
-import com.gryffindor.excalibur.repository.UserRepository;
-import java.util.Date;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+// import com.gryffindor.excalibur.constants.OrderStatus;
+// import com.gryffindor.excalibur.models.OrderRequest;
+// import com.gryffindor.excalibur.models.db.Order;
+// import com.gryffindor.excalibur.models.db.OrderDetails;
+// import com.gryffindor.excalibur.models.db.Product;
+// import com.gryffindor.excalibur.repository.OrderRepository;
+// import com.gryffindor.excalibur.repository.UserRepository;
+// import java.util.Date;
+// import java.util.List;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class OrderService {
-  private final OrderRepository orderRepository;
-  private final UserRepository userRepository;
-  private final MemberIdentityHandlerService memberIdentityHandlerService;
+// @Service
+// public class OrderService {
+//   private final OrderRepository orderRepository;
+//   private final UserRepository userRepository;
+//   private final MemberIdentityHandlerService memberIdentityHandlerService;
 
-  @Autowired
-  OrderService(
-      OrderRepository orderRepository,
-      UserRepository userRepository,
-      MemberIdentityHandlerService memberIdentityHandlerService) {
-    this.orderRepository = orderRepository;
-    this.userRepository = userRepository;
-    this.memberIdentityHandlerService = memberIdentityHandlerService;
-  }
+//   @Autowired
+//   OrderService(
+//       OrderRepository orderRepository,
+//       UserRepository userRepository,
+//       MemberIdentityHandlerService memberIdentityHandlerService) {
+//     this.orderRepository = orderRepository;
+//     this.userRepository = userRepository;
+//     this.memberIdentityHandlerService = memberIdentityHandlerService;
+//   }
 
-  public ResponseEntity<List<Order>> getAllOrders() {
-    try {
-      List<Order> orders = orderRepository.findAll();
-      if (orders.isEmpty()) {
-        return ResponseEntity.noContent().build();
-      }
-      return ResponseEntity.ok(orders);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
+//   public ResponseEntity<List<Order>> getAllOrders() {
+//     try {
+//       List<Order> orders = orderRepository.findAll();
+//       if (orders.isEmpty()) {
+//         return ResponseEntity.noContent().build();
+//       }
+//       return ResponseEntity.ok(orders);
+//     } catch (Exception e) {
+//       throw new RuntimeException(e);
+//     }
+//   }
 
-  public ResponseEntity<Order> getOrderById(String id) {
-    try {
-      Order order = orderRepository.findById(id).orElse(null);
-      if (order == null) {
-        return ResponseEntity.notFound().build();
-      }
-      return ResponseEntity.ok(order);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
+//   public ResponseEntity<Order> getOrderById(String id) {
+//     try {
+//       Order order = orderRepository.findById(id).orElse(null);
+//       if (order == null) {
+//         return ResponseEntity.notFound().build();
+//       }
+//       return ResponseEntity.ok(order);
+//     } catch (Exception e) {
+//       throw new RuntimeException(e);
+//     }
+//   }
 
-  @Transactional
-  public ResponseEntity<String> addOrder(OrderRequest orderRequest) {
-    try {
-      Order order = new Order();
-      order.setDate(new Date());
-      order.setOrderStatus(OrderStatus.PENDING);
-      userRepository
-          .findById(memberIdentityHandlerService.getLoggedInMemberID())
-          .ifPresent(order::setUser);
-      order.setOrderTotal(orderRequest.getOrderTotal());
-      List<OrderDetails> orderDetails =
-          orderRequest.getProduct().stream()
-              .map(
-                  orderItem -> {
-                    OrderDetails orderDetail = new OrderDetails();
-                    Product product = new Product();
-                    product.setId(orderItem.getProductId());
-                    orderDetail.setQuantity(orderItem.getQuantity());
-                    orderDetail.setProduct(product);
-                    orderDetail.setTotal(500L);
-                    return orderDetail;
-                  })
-              .toList();
-      order.setOrderDetails(orderDetails);
+//   @Transactional
+//   public ResponseEntity<String> addOrder(OrderRequest orderRequest) {
+//     try {
+//       Order order = new Order();
+//       order.setDate(new Date());
+//       order.setOrderStatus(OrderStatus.PENDING);
+//       userRepository
+//           .findById(memberIdentityHandlerService.getLoggedInMemberID())
+//           .ifPresent(order::setUser);
+//       order.setOrderTotal(orderRequest.getOrderTotal());
+//       List<OrderDetails> orderDetails =
+//           orderRequest.getProduct().stream()
+//               .map(
+//                   orderItem -> {
+//                     OrderDetails orderDetail = new OrderDetails();
+//                     Product product = new Product();
+//                     product.setId(orderItem.getProductId());
+//                     orderDetail.setQuantity(orderItem.getQuantity());
+//                     orderDetail.setProduct(product);
+//                     orderDetail.setTotal(500L);
+//                     return orderDetail;
+//                   })
+//               .toList();
+//       order.setOrderDetails(orderDetails);
 
-      orderRepository.save(order);
-      return new ResponseEntity<>("Order Placed Successfully", HttpStatus.OK);
-    } catch (Exception e) {
-      throw new RuntimeException(e.getMessage());
-    }
-  }
+//       orderRepository.save(order);
+//       return new ResponseEntity<>("Order Placed Successfully", HttpStatus.OK);
+//     } catch (Exception e) {
+//       throw new RuntimeException(e.getMessage());
+//     }
+//   }
 
-  public ResponseEntity<List<Order>> getOrdersForCustomer() {
-    try {
-      List<Order> orders =
-          orderRepository.getOrderByUserId(memberIdentityHandlerService.getLoggedInMemberID());
-      if (orders.isEmpty()) {
-        return ResponseEntity.noContent().build();
-      }
-      return ResponseEntity.ok(orders);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-}
+//   public ResponseEntity<List<Order>> getOrdersForCustomer() {
+//     try {
+//       List<Order> orders =
+//           orderRepository.getOrderByUserId(memberIdentityHandlerService.getLoggedInMemberID());
+//       if (orders.isEmpty()) {
+//         return ResponseEntity.noContent().build();
+//       }
+//       return ResponseEntity.ok(orders);
+//     } catch (Exception e) {
+//       throw new RuntimeException(e);
+//     }
+//   }
+// }
--- src/main/java/com/gryffindor/excalibur/services/usersService.java
+++ src/main/java/com/gryffindor/excalibur/services/usersService.java
@@ -1,71 +1,71 @@
-package com.gryffindor.excalibur.services;
+// package com.gryffindor.excalibur.services;
 
-import com.gryffindor.excalibur.constants.Roles;
-import com.gryffindor.excalibur.models.RegisterUser;
-import com.gryffindor.excalibur.models.db.User;
-import com.gryffindor.excalibur.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Validator;
-import java.util.List;
-import java.util.Set;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+// import com.gryffindor.excalibur.constants.Roles;
+// import com.gryffindor.excalibur.models.RegisterUser;
+// import com.gryffindor.excalibur.models.db.User;
+// import com.gryffindor.excalibur.repository.UserRepository;
+// import jakarta.persistence.EntityNotFoundException;
+// import jakarta.validation.ConstraintViolation;
+// import jakarta.validation.ConstraintViolationException;
+// import jakarta.validation.Validator;
+// import java.util.List;
+// import java.util.Set;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class usersService {
-  private final UserRepository userRepository;
-  // private final PasswordEncoder passwordEncoder;
-  private final Validator validator;
+// @Service
+// public class usersService {
+//   private final UserRepository userRepository;
+//   // private final PasswordEncoder passwordEncoder;
+//   private final Validator validator;
 
-  @Autowired
-  usersService(UserRepository userRepository, Validator validator) {
-    this.userRepository = userRepository;
-    this.validator = validator;
-  }
+//   @Autowired
+//   usersService(UserRepository userRepository, Validator validator) {
+//     this.userRepository = userRepository;
+//     this.validator = validator;
+//   }
 
-  @Transactional
-  public ResponseEntity<String> addUser(RegisterUser request, Roles role) {
-    User existingUserByUserName = userRepository.findByUserName(request.getUsername()).orElse(null);
-    if (existingUserByUserName != null) {
-      return new ResponseEntity<>("Customer already exists", HttpStatus.BAD_REQUEST);
-    }
+//   @Transactional
+//   public ResponseEntity<String> addUser(RegisterUser request, Roles role) {
+//     User existingUserByUserName = userRepository.findByUserName(request.getUsername()).orElse(null);
+//     if (existingUserByUserName != null) {
+//       return new ResponseEntity<>("Customer already exists", HttpStatus.BAD_REQUEST);
+//     }
 
-    User user = new User();
-    user.setUserName(request.getUsername());
-    user.setFirstName(request.getFirstName());
-    user.setLastName(request.getLastName());
-    user.setRole(role);
-    user.setDateOfBirth(request.getDateOfBirth());
-    user.setPassword(request.getPassword());
+//     User user = new User();
+//     user.setUserName(request.getUsername());
+//     user.setFirstName(request.getFirstName());
+//     user.setLastName(request.getLastName());
+//     user.setRole(role);
+//     user.setDateOfBirth(request.getDateOfBirth());
+//     user.setPassword(request.getPassword());
 
-    Set<ConstraintViolation<User>> violations = validator.validate(user);
-    if (!violations.isEmpty()) {
-      throw new ConstraintViolationException(violations);
-    }
+//     Set<ConstraintViolation<User>> violations = validator.validate(user);
+//     if (!violations.isEmpty()) {
+//       throw new ConstraintViolationException(violations);
+//     }
 
-    userRepository.save(user);
-    return new ResponseEntity<>("Registered Successfully", HttpStatus.OK);
-  }
+//     userRepository.save(user);
+//     return new ResponseEntity<>("Registered Successfully", HttpStatus.OK);
+//   }
 
-  public ResponseEntity<User> getCustomer(final String id) {
-    User user =
-        userRepository
-            .findById(id)
-            .orElseThrow(
-                () -> new EntityNotFoundException("Customer with id " + id + " not found"));
-    return ResponseEntity.ok(user);
-  }
+//   public ResponseEntity<User> getCustomer(final String id) {
+//     User user =
+//         userRepository
+//             .findById(id)
+//             .orElseThrow(
+//                 () -> new EntityNotFoundException("Customer with id " + id + " not found"));
+//     return ResponseEntity.ok(user);
+//   }
 
-  public ResponseEntity<List<User>> getAllCustomers() {
-    List<User> users = userRepository.findAll();
-    if (users.isEmpty()) {
-      throw new EntityNotFoundException("No Customers were found");
-    }
-    return ResponseEntity.ok(users);
-  }
-}
+//   public ResponseEntity<List<User>> getAllCustomers() {
+//     List<User> users = userRepository.findAll();
+//     if (users.isEmpty()) {
+//       throw new EntityNotFoundException("No Customers were found");
+//     }
+//     return ResponseEntity.ok(users);
+//   }
+// }
